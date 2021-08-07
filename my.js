27440b5cfffbe0f3932c3a91c1f09ef03ac5d1ae
samod singh <samod0098@gmail.com>
2021-08-07 13:38:15 +0530

    years-minute-second

---
+++ my.js
@@ -0,0 +1,38 @@
+function convertYeartoDay(Year){
+    return Math.floor(Year*365);
+
+}
+document.write(convertYeartoDay(65))
+
+document.write("<br>")
+
+document.write("<br>")
+document.write("<br>")
+
+
+function convertYeartoDay(Year){
+    return Math.floor(Year*365);
+
+}
+document.write(convertYeartoDay(20))
+
+document.write("<br>")
+
+
+document.write("<br>")
+document.write("<br>")
+
+
+function convertYeartoDay(Year){
+    return Math.floor(Year*365);
+
+}
+document.write(convertYeartoDay(0))
+
+document.write("<br>")
+
+
+
+
+
+
